f3ceb6cc686e66b7d6fd309321f6866ddbe16c7c
Nikhil Patwardhan <nikhil@nikhil-ThinkPad-SL410.(none)>
2010-11-20 16:34:27 -0500

    Modified struct source

--- go_filebench-1.4.8.fsl.0.7/sources.h
+++ go_filebench-1.4.8.fsl.0.7/sources.h
@@ -3,9 +3,9 @@
 struct source {
 	char* s_name;
 	double s_entropy;
-	struct source_operations *sops; 
+	struct source_operations *s_ops; 
 };
 
 struct source_operations{
-	int (*fill)(struct source *, void *buf);
+	int (*fill)(struct source *, void *, unsigned int);
 };
