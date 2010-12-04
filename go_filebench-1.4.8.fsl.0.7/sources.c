46a09c853a775cf6e6609c0567a559ead67af647
aboSamoor <rmyeid@gmail.com>
2010-12-04 17:08:05 -0500

    refactoring the code, and adding the lookup_fill method, permutate_method

--- go_filebench-1.4.8.fsl.0.7/sources.c
+++ go_filebench-1.4.8.fsl.0.7/sources.c
@@ -53,10 +53,13 @@ int constant_fill(struct source *ds, void *buf, unsigned int size){
 }
 
 /*
-	This function fills the buffer with random data according to
-	a pdf has the entropy specified in the source.
+	_search_fill fills the buffer with random data according to
+	a pdf has the entropy specified in the source. It calculates
+    the cdf then populate the buffer by searching in the cdf for 
+    the random number. Sometimes this approach is called roulette
+    algorithm. The binary search is expected to add 8x factor.
 */
-int entropy_accurate_fill(struct source *ds, void *buf, unsigned int size){
+int entropy_search_fill(struct source *ds, void *buf, unsigned int size){
 	
 	int i = 0;
 	double pdf[PDF_SIZE];
@@ -79,23 +82,22 @@ int entropy_accurate_fill(struct source *ds, void *buf, unsigned int size){
 	//shuffle the symbols table
 	permutate(symbols_table, PDF_SIZE);
 
-	//filling the buffer depending on the cdf
-	char *tmp = malloc(size);
 	for(i=0; i < size; i++){
-		tmp[i] = symbols_table[binary_search(rand()/(double)RAND_MAX, cdf, PDF_SIZE)];
+		((unsigned char*)buf)[i] = symbols_table[binary_search(rand()/(double)RAND_MAX, cdf, PDF_SIZE)];
 	}
-	memcpy(buf, tmp, size);
-	free(tmp);
 	return 0;
 }
 
-
 /*
-	This function fills the buffer with random data according to
-	a pdf has with different populating algorithm than entropy_fill
-	it takes less time but it does not give homogeous entropy data stream
+	_cont_fill fills the buffer with random data according to
+	the pdf. It will generate contigouos segements of data in the
+    buffer to make different same size buffer look different, we
+    shuffle the symbols table. It takes less time but it does not
+    give homogeous entropy data stream. Because of rounding errors
+    we will have some remaining elements are not filled, so we will
+    use the search method to fill them.
 */
-int entropy_fill(struct source *ds, void *buf, unsigned int size){
+int entropy_cont_fill(struct source *ds, void *buf, unsigned int size){
 	
 	int i = 0;
 	double pdf[PDF_SIZE];
@@ -112,13 +114,12 @@ int entropy_fill(struct source *ds, void *buf, unsigned int size){
 	//Calculate cdf from the pdf
 	calculate_cdf(pdf, PDF_SIZE, cdf);
 
-	void *tmp = malloc(size);
 	//initializing the symbol table
 	for(i=0; i< PDF_SIZE; i++)
 		symbols_table[i] = (unsigned char)i;
 
-	//shuffle the symbols table
-	permutate(symbols_table, PDF_SIZE);
+	//shuffle the buffer
+    permutate(symbols_table, PDF_SIZE);
 
 	int k=0;
 	int j=0;
@@ -126,19 +127,75 @@ int entropy_fill(struct source *ds, void *buf, unsigned int size){
 	for(i=0; i< PDF_SIZE; i++){
 		//printf("values %u %f %u",i,pdf[i],size);
 		for(j=0; j < (int)(pdf[i]*size); j++){
-			((unsigned char*)tmp)[k] = symbols_table[i];
+			((unsigned char*)buf)[k] = symbols_table[i];
 			k++;
 		}
 	}
 	//remaining elements
 	for(; k < size; k++){
-		((unsigned char *)tmp)[k] = symbols_table[binary_search(rand()/(double)RAND_MAX, cdf, PDF_SIZE)];
+		((unsigned char *)buf)[k] = symbols_table[binary_search(rand()/(double)RAND_MAX, cdf, PDF_SIZE)];
 	}
-	memcpy(buf, tmp, size);
-	free(tmp);
+    
 	return 0;
 }
 
+/*
+	_permutate_fill fills the buffer with random data according to
+	the pdf. It will generate contigouos segements of data in the
+    buffer. After that a permutation function will be called to make
+    the buffer looks with homogenous entropy. The permutation
+    function has high overhead, because it is not cache friendly.
+    tests show 16x overhead
+    
+*/
+
+int entropy_permutate_fill(struct source *ds, void *buf, unsigned int size){
+    int err = entropy_cont_fill(ds, buf, size);
+    permutate((unsigned char*)buf,size); 
+	return err;
+}
+
+/*
+    _lookup_fill initializes a vector using _*_fill method then
+    using that vector we will initilize our buffer by lookuping
+    differnt elements using a random index. 
+*/
+
+int entropy_lookup_fill(struct source *ds, void *buf, unsigned int size){
+	
+	int i = 0;
+
+    //initilize the vector
+    int lookup_size = 16*1024;
+    void *lookup_table = malloc(lookup_size);
+    int err = entropy_cont_fill(ds, lookup_table,lookup_size);
+
+#ifndef _FB_FILEBENCH_H
+	printf("The entropy of the lookup table: %f\n", buf_entropy(lookup_table, lookup_size));
+#endif
+	for(i=0; i <size ; i++){
+		((unsigned char*)buf)[i] = ((unsigned char*)lookup_table)[rand()%lookup_size];
+		}
+    
+    free(lookup_table);
+    //copy the tmp buffer to the target one
+	return err;
+}
+
+/*
+    This function will create a tmp buffer and call the actual
+    function to fill the tmp buffer with the specific entropy.
+    Creating a tmp buffer is because of a weird error happens
+    in filebench in case we manipulate the original buffer directly.
+*/
+
+int entropy_fill(struct source *ds, void *buf, unsigned int size){
+	void *tmp = malloc(size);
+    int err = entropy_cont_fill(ds, tmp, size);
+    memcpy(buf, tmp, size);
+    free(tmp);
+    return err;
+}
 
 struct source_operations constant_operations = {
 	.fill = constant_fill,
@@ -158,7 +215,7 @@ struct source_operations dummy_operations = {
 int main(int argc, char **argv){
 	
 	struct source ds;
-	unsigned int size = 1*1024*1024;
+	unsigned int size = 256*1024*1024;
 	unsigned char* buf=malloc(size);
 	clock_t start = clock();
 	ds.s_entropy = 7.84;
