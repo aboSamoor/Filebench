0248a3d56827adb115efbd720ed367ecbcd1c346
aboSamoor <rmyeid@gmail.com>
2010-12-04 20:22:29 -0500

    changin the process script to accomodate the existence of robot.py

--- go_filebench-1.4.8.fsl.0.7/process.sh
+++ go_filebench-1.4.8.fsl.0.7/process.sh
@@ -1,48 +1,38 @@
 #! /bin/bash
 
-home=$HOME
-prog="${PWD}/go_filebench -f"
-write_load="${PWD}/workloads/rami_write.f"
-read_load="${PWD}/workloads/rami_read.f"
-fset_dir="/media/sda7/rami_fileset"
-
-#rw_load="${PWD}/workloads/rami_rw.f"
+prog="$1 -f"
+load="$2"
+fset_dir="$3"
+ent="$4"
 
 change_dir="cd ${PWD}"
 cmd_del_fset="rm -rf ${fset_dir}"
-cmd_write="${prog} ${write_load}"
-cmd_read="${prog} ${read_load}"
-cmd_mv="cp -r ${PWD}/rami_* /home/eid/Desktop/ ; rm -rf ${PWD}/rami_*"
-testing="${HOME}/ent/ent ${fset_dir}/00000001/00000001"
+cmd_load="${prog} ${load}"
+cmd_testing="${ent} ${fset_dir}/00000001/00000001"
+cmd_size="du -s ${fset_dir}"
 
 echo "@@@@@@@@@@@@@@@@@@This script expects to run as a superuser, to switch to a superuser run 'sudo -i'@@@@@@@@@@@@@@"
 echo "@@@@@@@@@@@@@@@@@@This script expects that you have  /root/ent/ent to test files                   @@@@@@@@@@@@@@"
 
-#sudo -i
-$change_dir
 echo "Deleting old filesets ...................."
+echo "$cmd_del_fset"
 $cmd_del_fset
 echo
 echo "Dropping memory caches...................."
+echo "sync && echo 3 > /proc/sys/vm/drop_caches"
 sync && echo 3 > /proc/sys/vm/drop_caches
 echo
 echo "Writing load.............................."
+echo "$cmd_load"
 stime=$(date +%s)
-$cmd_write
+$cmd_load
 etime=$(date +%s)
 echo "Time spent in Filebench is $((etime - stime)) seconds"
 echo
 echo "Testing files.............................."
-$testing
+echo "$cmd_testing"
+$cmd_testing
+echo "Size of the fileset on the partition......."
+echo "$cmd_size"
+$cmd_size
 echo
-#echo "Dropping memory caches...................."
-#sync && echo 3 > /proc/sys/vm/drop_caches
-#echo
-#echo "Reading load.............................."
-#$cmd_read
-#echo
-#echo "Testing files.............................."
-#$testing
-#echo
-#echo "Moving log files.........................."
-#$cmd_mv
